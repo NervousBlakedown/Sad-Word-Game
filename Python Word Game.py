b01ddb89b4b60efd840c16f634b4f1e15740b4ea
Blake Calhoun <blakecalhoun35@gmail.com>
2017-11-11 05:31:11 -0600

    Add files via upload

--- Python Word Game.py
+++ Python Word Game.py
@@ -1,9 +1,28 @@
-print "Hello.  Welcome to the depressing adventure word game.  Life is meaningless.""
+#Begin/Name and Age Validation
+print "Hello.  Welcome to the depressing adventure word game.  Life is meaningless."
 name = raw_input("What is your name?");
 if len(name) > 0 and name.isalpha():
-    print "I am glad you are choosing to proceed, {name}.  Enjoy yourself.  It will be difficult."
+    print "I am glad you are choosing to proceed, {name}.  Welcome.  I will forget your name, but do not take it personally, even though it is very personal."
 else:
     print "My apologies, {name}.  It appears your name sucks and does not qualify as a real name.  Please change your name to proceed."
+        if len(name) > 0 and name.isalpha():
+            print "Ah, finally, {name}, thank you for not being a smart-ass anymore.  Welcome."
+
+print "Now that we know your name, we must confirm your age is eighteen years or older.  This game is not inappropriate, we just feel safer knowing you are old enough to be held responsible for all of your stupid decisions."
+age = raw_input("What is your age?");
+if len(age) < 18:
+    print "I am sorry, but you cannot proceed.  You are far too young.  I can still smell breast milk on your breath.  Please go outside and play with a sports ball or something."
+else:
+    print "Really?  {age}?  Prove it."
+#insert funny way of checking date of birth, then asking something like 'how old were you when NSync's first single came out' or 'what was your mom wearing in 2003'
+
+print "Okay, {name}, age {age}, let's begin the story."
+print "It is a boring day like all the others.  You wake up at 5:30 A.M.  An ungodly hour."
+place = raw_input("Where do you go first?")
+#find way to list options (dropdown or lettered multiple choice), options: "work", "school", "brothel"
+if place = "Work"
+print "Ah."
+
 
 print "Oh, look, a pig latin translator. Do be careful.  It is a cold, cruel world out there."
 #Pig Latin translator
